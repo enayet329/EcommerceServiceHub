a5e006c3ec9de700ea3c86badad7c4465126edbb
MD Anayet Hossen <mdsakayethossen2@gmail.com>
2024-10-04 23:47:56 +0600

    feat: Implement IProduct Repository and perform CRUD operation

---
+++ src/Services/Catalog/Catalog.API/Repository/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(string id);
+        Task<Product> GetProductByNameAsync(string name);
+        Task<IEnumerable<Product>> GetProductByCategoryAsync(string category);
+
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProductAsync(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
